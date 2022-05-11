2280dcfae65a314572372b0b10e085364028d0b8
Arnas Jelizarovas <jelizarovas@gmail.com>
2022-05-11 08:56:48 -0700

    added dates, & sfm

--- src/components/Employment.tsx
+++ src/components/Employment.tsx
@@ -20,7 +20,7 @@ type JobType = {
   location: string;
   company: string;
   startDate: string;
-  leaveDate: string;
+  leaveDate?: string;
   description: string[];
   logo: string;
 };
@@ -39,8 +39,18 @@ const Job = ({ job }: { job: JobType }) => {
           <span className="text-xs whitespace-nowrap uppercase text-slate-500">
             {job.company}
           </span>
+          <div className="text-sm">
+            {parseDateString(job.startDate)}{" "}
+            {job?.leaveDate
+              ? `to ${parseDateString(job.leaveDate)}`
+              : "- Current"}
+          </div>
+          <div className="text-gray-500 text-xs">
+            {timeEmployed(job.startDate, job?.leaveDate || undefined)}
+          </div>
         </div>
       </div>
+
       <div className="flex flex-col items-stretch">
         <div className="flex flex-col">
           {job.description.map((duty, i) => (
@@ -53,3 +63,40 @@ const Job = ({ job }: { job: JobType }) => {
     </div>
   );
 };
+
+function parseDateString(date: string) {
+  const d = new Date(date);
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return `${months[d.getMonth()]}, ${d.getFullYear()}`;
+}
+
+function timeEmployed(dateStart: string, dateLeave?: string) {
+  const start = new Date(dateStart);
+  const end = dateLeave ? new Date(dateLeave) : new Date();
+
+  const duration = end.valueOf() - start.valueOf();
+
+  const s = duration / 1000;
+  const min = s / 60;
+  const h = min / 60;
+  const days = h / 24;
+  const months = Math.ceil(days / 30);
+
+  return `${months > 11 ? Math.floor(months / 12) + " year," : ""} ${
+    months % 12
+  } months`;
+}
